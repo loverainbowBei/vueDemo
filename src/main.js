7cf4cbd44aa0cffa00cbfacd3bc83a69d25746f9
RunLi <907918499@qq.com>
2019-04-16 21:43:49 +0800

    静态的登录页面完成

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import ElementUI from 'element-ui'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
 
 //引入index.js 文件
 import './styles/index.scss'
+//注册插件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
