46e773dd47a82b9d67de699f60da62757646f2c4
RunLi <907918499@qq.com>
2019-04-18 14:12:50 +0800

    整整体布局完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    主页已经跳到舍得
+  <div class="home">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script>
@@ -16,6 +22,40 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+  .home {
+    height: 100%;
+  }
+  .el-container{
+    height: 100%;
+  }
+  .el-aside{
+    background-color: #545c64;
+  }
+  .el-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #545c64;
+  }
+  .logo {
+    height: 60px;
+    background: url('../assets/logo.png');
+     //把背景图像扩展至足够大，以使背景图像完全覆盖背景区域。
+    background-size: cover;  
+    background-color: #989898;
+  }
+  .toggle-btn{
+    font-size: 36px;
+    color: #989898;
+    cursor: pointer;
+    line-height: 60px;
+  }
+  .header-title{
+    font-size: 28px;
+    color: #fff;
+  }
+  .logout-btn{
+    color: orange;
+  }
 </style>
 
