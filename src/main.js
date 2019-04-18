305cd9a7701321b01167364c266c89ca2f78bec4
RunLi <907918499@qq.com>
2019-04-18 13:14:59 +0800

    注册个路由导航卫士

--- src/main.js
+++ src/main.js
@@ -13,7 +13,20 @@ Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// 注册一个全局守卫（导航卫士）,其作用就是在路由跳转之前，对路由进行判断，防止未登录的用户跳转到需要登录的页面
+router.beforeEach((to, from, next) => {
+        let token = localStorage.getItem('mytoken') //获取用户登录时生成的token
+        if (token) {
+            next(); //如果token存在，说明用户已经登录，调用next方法，不妨碍用户继续浏览
+        } else {
+            if (to.path !== '/login') { //如果用户没有登录浏览别的页面，则强制跳转到登录页面
+                next({ path: '/login' })
+            } else { //如果用户浏览的就是登录页面，则调用next方法 ，不去干涉，
+                next()
+            }
+        }
+    })
+    /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
